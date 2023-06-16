18a8ca6eba33ef6707c49d72b9ff48a57f40c016
Mouizeddine <mouizm165@gmail.com>
2023-06-16 20:33:19 +0100

    Pk and Alex are cute

--- index.js
+++ index.js
@@ -21,13 +21,11 @@ const newsArticles = [
 // prettier-ignore
 const keywords = 'a:contains("crypto"), a:contains("Crypto"), a:contains("cryptocurrency"), a:contains("Cryptocurrency"), a:contains("web3"), a:contains("Web3"), a:contains("blockchain"), a:contains("Blockchain"), a:contains("bitcoin"), a:contains("Bitcoin"), a:contains("ethereum"), a:contains("Ethereum"), a:contains("binance"), a:contains("Binance"), a:contains("coinbase"), a:contains("Coinbase"), a:contains("SEC"), a:contains("xrp"),  a:contains("XRP"), a:contains("ripple"), a:contains("Ripple"), a:contains("solana"), a:contains("Solana"), a:contains("shiba"), a:contains("Shiba"), a:contains("coin"), a:contains("AI"), a:contains("invest"), a:contains("trade"), a:contains("trading"), a:contains("investing"), a:contains("market"), a:contains("decentralized"), a:contains("ICO"), a:contains("wallet"), a:contains("smart contract"), a:contains("exchange"), a:contains("DeFi"), a:contains("DApp"), a:contains("stake"), a:contains("CZ"), a:contains("whales"), a:contains("Bull market"), a:contains("bull market"), a:contains("Bear market"), a:contains("bear market")'
 
-let articles = [];
+const articles = [];
 // ...
 
 // Function to fetch articles and populate the articles array
 const fetchArticles = async () => {
-  articles = [];
-
   for await (const newsArticle of newsArticles) {
     let sourceArticles = []; // Separate array for each source
 
@@ -54,13 +52,18 @@ const fetchArticles = async () => {
             const img = articleElement.find("img");
             const imageUrl = img.attr("src");
 
-            sourceArticles.push({
+            const payload = {
               title,
               imageUrl,
               url: newsArticle.base + url,
               source: newsArticle.name,
-            });
-            // });
+            };
+
+            const doubled = articles.find((element) => element === payload);
+            if (doubled) {
+              return;
+            }
+            articles.push(payload);
           });
       })
       .catch((error) => console.log(error));
@@ -86,3 +89,44 @@ app.listen(PORT, () => {
 });
 
 module.exports = app;
+
+// const fetchArticles = async () => {
+//   for await (const newsArticle of newsArticles) {
+//     await axios
+//       .get(newsArticle.address)
+//       .then((response) => {
+//         const html = response.data;
+//         const $ = cheerio.load(html);
+//         let imageUrl;
+
+//         $("a", html).each(function (index) {
+//           const title = $(this).text();
+//           const url = $(this).attr("href");
+
+//           if (
+//             newsArticle.address ===
+//             "https://techcrunch.com/category/cryptocurrency/"
+//           ) {
+//             const articleElements = $(".post-block");
+//             const articleElement = articleElements.eq(index);
+//             const img = articleElement.find("footer img");
+//             imageUrl = img.attr("src");
+//           }
+//           const payload = {
+//             title,
+//             imageUrl,
+//             url: newsArticle.base + url,
+//             source: newsArticle.name,
+//           };
+
+//           const doubled = articles.find((element) => element === payload);
+//           if (doubled) {
+//             return;
+//           }
+//           articles.push(payload);
+//         });
+//       })
+//       .catch((error) => console.log(error));
+//   }
+//   return articles;
+// };
